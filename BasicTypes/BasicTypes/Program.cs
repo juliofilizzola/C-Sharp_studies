e5b38ac0c144aa98a6bf1c62d93e56a78e2dd2e1
Filizzola <juliofilizzola@hotmail.com>
2022-02-11 16:20:00 -0300

    ✨: os exemplo

--- BasicTypes/BasicTypes/Program.cs
+++ BasicTypes/BasicTypes/Program.cs
@@ -11,12 +11,17 @@ namespace BasicTypes
             sbyte x = 127;
             x++; // como o sByte já está no máximo permitido, ele vai para o oposto.
             char y = 'a';
+            char c = '\u0041';
             string s = "X referece a sbyte e y se refere a char";
+            bool thisIsBool = false;
+            string thisIsString = "This is string";
+            // a variavel que for cria como string, deve conter ela com aspas duplas
+            Console.WriteLine(thisIsBool);
+            Console.WriteLine(thisIsString);
+            Console.WriteLine(c);
             Console.WriteLine(x);
             Console.WriteLine(y);
             Console.WriteLine(s);
-
-
         }
     }
 }
