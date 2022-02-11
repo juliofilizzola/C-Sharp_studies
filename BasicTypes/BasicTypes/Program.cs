fffdfa9e57c8dced9fa19c86efb9da71eeffd3af
Filizzola <juliofilizzola@hotmail.com>
2022-02-11 12:19:50 -0300

    ✨: primeiros exemplos

--- BasicTypes/BasicTypes/Program.cs
+++ BasicTypes/BasicTypes/Program.cs
@@ -7,6 +7,13 @@ namespace BasicTypes
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            sbyte x = 100;
+            char y = 'a';
+            string s = "X referece a sbyte e y se refere a char";
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(s);
         }
     }
 }
