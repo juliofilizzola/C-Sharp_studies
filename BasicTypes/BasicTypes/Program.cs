20dd3510c87e70cd46f860f265dc52a2392ee166
Filizzola <juliofilizzola@hotmail.com>
2022-02-11 12:27:12 -0300

    ✨: exemplo de sbyte e n++

--- BasicTypes/BasicTypes/Program.cs
+++ BasicTypes/BasicTypes/Program.cs
@@ -8,12 +8,15 @@ namespace BasicTypes
         {
             Console.WriteLine("Hello World!");
 
-            sbyte x = 100;
+            sbyte x = 127;
+            x++; // como o sByte já está no máximo permitido, ele vai para o oposto.
             char y = 'a';
             string s = "X referece a sbyte e y se refere a char";
             Console.WriteLine(x);
             Console.WriteLine(y);
             Console.WriteLine(s);
+
+
         }
     }
 }
