fa7bc9116706a63f126f4994716a21da558961c5
Filizzola <juliofilizzola@hotmail.com>
2022-02-20 16:51:18 -0300

    :hammer: criando exempo de saida de função

---
+++ saidaDeFunção/saidaDeFunção/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace saidaDeFunção
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
