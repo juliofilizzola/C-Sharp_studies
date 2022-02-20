e69d0bf935409af5517c361219944928b6fbd8e2
Filizzola <juliofilizzola@hotmail.com>
2022-02-20 17:00:50 -0300

    :hammer: Exemplos de template string

--- saidaDeFunção/saidaDeFunção/Program.cs
+++ saidaDeFunção/saidaDeFunção/Program.cs
@@ -8,7 +8,10 @@ namespace saidaDeFunção {
             double sold = 10.35784;
             string name = "Maria";
 
-            Console.WriteLine("{0} tem {1} anos e tem o saldo igual a {2:F2}", name, age, sold);
+            Console.WriteLine("{0} tem {1} anos e tem o saldo igual a {2:F2} reais", name, age, sold);
+            // essa saida é o placeholder
+
+            Console.WriteLine($"{name} tem {age} anos e tem o saldo igual a {sold:F2} reais");
         }
     }
 }
