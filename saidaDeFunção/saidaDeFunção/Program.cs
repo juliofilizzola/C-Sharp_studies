f8f5374b319d327fed96454607a9fa7fed94bab1
Filizzola <juliofilizzola@hotmail.com>
2022-02-20 16:57:47 -0300

    :hammer: Exemplos de saída de dados em c#

--- saidaDeFunção/saidaDeFunção/Program.cs
+++ saidaDeFunção/saidaDeFunção/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
-namespace saidaDeFunção
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace saidaDeFunção {
+    class Program {
+        static void Main(string[] args) {
+
+            int age = 32;
+            double sold = 10.35784;
+            string name = "Maria";
+
+            Console.WriteLine("{0} tem {1} anos e tem o saldo igual a {2:F2}", name, age, sold);
         }
     }
 }
